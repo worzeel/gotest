2cd4c8ff4548b900a27df9c312562132fb6aed2f
Antonio Broughton <antonio@flerwin.com>
2015-12-12 11:40:58 +1300

    Added some more stuff... also trying out git-plus package in atom
    so far seems to be quite the interesting :)

--- main.go
+++ main.go
@@ -5,32 +5,91 @@ import (
 	"fmt"
 )
 
-type MyTest struct {
-	name string
-	age int
+var i = 2
+
+// MyTestStruct test
+type MyTestStruct struct {
+	X int
+	Y int
 }
 
-func (self MyTest) random() MyTest {
-	self.name = "Random"
-	//self.age = 123
-	
-	return self
+// SetValues testing
+func (m *MyTestStruct) SetValues(x, y int) {
+	m.X = x
+	m.Y = y
 }
 
-func (self MyTest) getThings() (string, int) {
-	return "from GetThings", 11
+// Testing is a test
+func (m MyTestStruct) Testing() int {
+	return m.X + m.Y
+}
+
+func myTestFunc(x, y int) int {
+	return x + y + i
+}
+
+func myTestFunc2(x, y int) (int, string) {
+	return x + y, "test"
+}
+
+// this type of return should only be used for very small methods for readability
+func myTestNamedFunc() (x, y int) {
+	x = 3
+	y = 5
+
+	// naked return
+	return
 }
 
 func main() {
-	test := MyTest{}
-	
-	test.age = 111
-	test.name = "test"
-	
-	test2 := test.random()
-	
-	getThingsStr, getThingsInt := test.getThings()
-	
-	fmt.Println("testing ", test.name, " ", test.age, " ", test2.name, " ", test2.age)
-	fmt.Println(getThingsStr, " - ", getThingsInt)
+	var aTest string
+	aTest = "test"
+
+	a, b := myTestFunc2(2, 3)
+	c, d := myTestNamedFunc()
+
+	fmt.Println("this is just a testing", myTestFunc(1, 2))
+	fmt.Println("second:", a, b)
+	fmt.Println("third:", c, d)
+	fmt.Println("fourth:", aTest)
+
+	sum := 0
+	for i := 0; i < 5; i++ {
+		if i%2 == 0 {
+			sum += i
+		}
+	}
+	fmt.Println("fifth:", sum)
+
+	switch test := 2; test {
+	case 1:
+		fmt.Println(1)
+
+	case 2:
+		fmt.Println(2)
+
+	default:
+		fmt.Println(test)
+	}
+
+	switch {
+	case a == 5:
+		fmt.Println("five")
+
+	default:
+		fmt.Println("something else")
+	}
+
+	myTest := MyTestStruct{}
+	myTest.SetValues(1, 2)
+	fmt.Println(myTest.Testing())
+
+	fmt.Println("Struct test", MyTestStruct{1, 2})
+
+	i := 1
+	p := &i
+	j := i + *p
+
+	fmt.Println("i:", i, "*p:", *p, "p:", p)
+	fmt.Println("j:", j)
 }
